3362ada40a5a5620f9551c17bb27215871e9244c
andre <duilioandrecastro@gmail.com>
2022-10-24 17:56:35 +0200

    feat: bootstrap implemented

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    console.log("constructor")
+  }
 
   ngOnInit(): void {
+    console.log("ngOnInit")
   }
 
 }
