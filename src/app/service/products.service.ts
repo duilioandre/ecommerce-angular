15c14f557944e187c5f1ab6f157eb059e033098d
andre <duilioandrecastro@gmail.com>
2022-11-06 18:54:26 +0100

    feat: product detail page

--- src/app/service/products.service.ts
+++ src/app/service/products.service.ts
@@ -8,12 +8,12 @@ export class ProductsService {
   constructor() { }
 
   private _products: Product[] = [
-    {code:"001", category:1, name:"prodotto 1", description:"description 1", picture:"", price:100},
-    {code:"002", category:2, name:"prodotto 2", description:"description 2", picture:"", price:200},
-    {code:"003", category:1, name:"prodotto 3", description:"description 3", picture:"", price:300},
-    {code:"004", category:2, name:"prodotto 4", description:"description 4", picture:"", price:400},
-    {code:"005", category:2, name:"prodotto 5", description:"description 5", picture:"", price:500},
-    {code:"006", category:1, name:"prodotto 6", description:"description 6", picture:"", price:250},
+    {code:"001", category:1, name:"prodotto 1", description:"description 1", picture:"../assets/placeholder_card_product.png", price:100},
+    {code:"002", category:2, name:"prodotto 2", description:"description 2", picture:"../assets/placeholder_card_product.png", price:200},
+    {code:"003", category:1, name:"prodotto 3", description:"description 3", picture:"../assets/placeholder_card_product.png", price:300},
+    {code:"004", category:2, name:"prodotto 4", description:"description 4", picture:"../assets/placeholder_card_product.png", price:400},
+    {code:"005", category:2, name:"prodotto 5", description:"description 5", picture:"../assets/placeholder_card_product.png", price:500},
+    {code:"006", category:1, name:"prodotto 6", description:"description 6", picture:"../assets/placeholder_card_product.png", price:250},
   ]
 
   get products() {
