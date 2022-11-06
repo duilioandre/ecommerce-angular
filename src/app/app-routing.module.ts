28b0ab55cdcab6c3f4739d4697fc7619eb97c94e
andre <duilioandrecastro@gmail.com>
2022-11-06 17:00:25 +0100

    feat: product detail page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './product/product.component';
+import { ProductsComponent } from './products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/products' ,pathMatch:'full'},
+  {path: 'products', component:ProductsComponent},
+  {path: 'products/product/:code', component:ProductComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../model/product.model';
+import { ProductsService } from '../service/products.service';
 
 @Component({
   selector: 'app-product',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  product?: Product;
+  productCode?: string | null
+
+
+  constructor(private activeRoute: ActivatedRoute, private productsService: ProductsService) { }
 
   ngOnInit(): void {
+    this.productCode = this.activeRoute.snapshot.paramMap.get('code')
+    this.product = this.productsService.products.find(p => p.code == this.productCode)
   }
 
 }
